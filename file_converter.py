21bf21a426f748965bfd30f338e225f3f4624a81
ColorLion <godownfight@naver.com>
2019-09-25 01:01:30 +0900

    initial commit

---
+++ file_converter.py
@@ -0,0 +1,17 @@
+import plugins.csv2excel
+import os
+
+# 작업 위치 및 저장 위치 정의
+def main():
+    input_dir = ""
+    print("work dir: " + input_dir)
+    output_dir = input_dir + '\\' + "output"
+
+    # input dir 유효성 검사
+    if os.path.isdir(input_dir) == 0:
+        print("디렉토리를 다시 지정해 주세요")
+        exit(0)
+
+    # output 저장 폴더 확인
+    if os.path.isdir(output_dir) == 0:
+        os.mkdir(output_dir)
---
+++ plugins/csv2excel.py
@@ -0,0 +1,4 @@
+import openpyxl
+
+# dir 내에 있는 csv파일을 excel파일로 변환
+
