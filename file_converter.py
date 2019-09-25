1833aea56e5d9ca26d28141432d36ca76ff0ca5d
ColorLion <godownfight@naver.com>
2019-09-26 00:48:36 +0900

    csv2excel start

--- file_converter.py
+++ file_converter.py
@@ -6,7 +6,10 @@ import os
 def main():
     # 작업 위치를 입력 받음
     input_dir = input("dir: ")
+    input_dir += '\\'
+    print("==============================")
     print("work dir: " + input_dir)
+    print("==============================")
 
     # input dir 유효성 검사
     if os.path.isdir(input_dir) == 0:
@@ -15,7 +18,10 @@ def main():
 
     # 변환하고자 하는 파일의 확장자 확인
     # 추후 변경(플러그인에서 변경)
-    print("convert 지원하는 확장자: csv to excel, excel to csv")
+    print("convert 지원 목록")
+    print("==============================")
+    print("csv to excel / excel to csv")
+    print("==============================")
 
     # 추후 변경(optparse 사용)
     src_ext = input("source file extension: ")
@@ -28,16 +34,22 @@ def main():
         pass
     else:
         print("변환하려는 확장자가 잘못되었거나 지원하지 않습니다")
+        exit(0)
 
     # output 저장 폴더 확인 및 생성
-    output_dir = input_dir + '\\' + "output"
+    output_dir = input_dir + "output" + '\\'
     if os.path.isdir(output_dir) == 0:
         os.mkdir(output_dir)
 
+    # 변환할 파일
+    files = os.listdir(input_dir)
+
     # 변환 함수 사용
     if ext == "csv2excel":
-        print("csv2excel")
+        plugins.csv2excel.csv2excel(input_dir, output_dir, files)
     elif ext == "excel2csv":
         print("excel2csv")
     else:
         print("이게 출력되면 안되긴 함")
+
+main()
--- plugins/csv2excel.py
+++ plugins/csv2excel.py
@@ -1,4 +1,25 @@
+# -*- coding:utf-8 -*-
 import openpyxl
+import codecs
 
 # dir 내에 있는 csv파일을 excel파일로 변환
+# ===============================================
+# csv2excel
+# parameter
+#   - input_dir:    변환할 파일 존재하는 디렉토리
+#   - output_dir:   변환된 파일이 저장될 디렉토리
+#   - files:        변환할 파일들
+# ===============================================
+def csv2excel(input_dir, output_dir, files):
+    print("input dir: " + input_dir)
+    print("output dir: " + output_dir)
 
+    # 폴더 내에 있는 xls 파일을 순차적으로 읽음
+    for file in files:
+        if len(file.split('.')) == 2:
+            if file.split('.')[1] == 'csv':
+                print(" - " + file)
+                # 파일 형식 지정
+                f = codecs.open(input_dir + file, 'r', encoding='utf8')
+                # CSV 데이터 정제
+                print(f.readline().replace('"', "").replace("\r\n", "").split(","))
